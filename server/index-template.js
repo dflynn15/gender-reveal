418712704e5cea87cae2d56752a43abea8dd1d7f
Daniel Flynn <djflynn15@gmail.com>
2018-10-11 22:40:31 -0400

    :lock: hack: Enforce HTTPS
    - Enforce HTTPS with redirect due to Google Domains not allowing ANAME or ALIST

--- server/index-template.js
+++ server/index-template.js
@@ -6,6 +6,13 @@ export default html =>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
 	<script src="/socket.io/socket.io.js"></script>
+  <script>
+    //Cue barfing
+    var host = "www.inlikebabyflynn.com";
+    if ((host == window.location.host) && (window.location.protocol != "https:")){
+      window.location.protocol = "https";
+    }
+  </script>
   <title>Gender Reveal</title>
 </head>
 
